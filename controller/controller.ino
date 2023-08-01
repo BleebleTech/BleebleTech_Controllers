559af78bf669bac3da02152c46eba877d6872d92
Matthew Allwright <matt.a.4890@gmail.com>
2023-07-31 19:35:26 -0600

    Create initial controller sketch

---
+++ controller/controller.ino
@@ -0,0 +1,16 @@
+/**
+ * File: controller.ino
+ * Author: Matthew Allwright, theBasicBot
+ * Copyright: 2023
+ *
+ * Description:
+ * This file serves as the base sketch for theBasicBot's ESP32-based controller.
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
