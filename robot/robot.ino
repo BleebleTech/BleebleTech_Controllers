f36beadbf162b206161a62bc5cf447b1d24d7994
Matthew Allwright <matt.a.4890@gmail.com>
2023-09-18 20:27:39 -0600

    Add basic robot serial setup

--- robot/robot.ino
+++ robot/robot.ino
@@ -15,13 +15,25 @@ static constexpr int kRightWheel_Forwards = 5;
 static constexpr int kLeftWheel_Forwards = 6;
 static constexpr int kLeftWheel_Backwards = 11;
 
+// Generic configuration
+static constexpr int kLoopDelay_ms = 16;
+
 /* Variables ------------------------------------------------------------------------------------ */
 
 /* Functions ------------------------------------------------------------------------------------ */
 
+static void processByteFromBle(const uint8_t aRxByte) {}
+
 /* Setup and Loop ------------------------------------------------------------------------------- */
 
 void setup() {
+  // Setup serial
+  Serial.begin(115200);
+  // NOTE: This serial port is shared between both the BLE adapter (e.g., HM-10 module), and the
+  // Serial Monitor that is available when you connect the Arduino to a computer. This means if you
+  // print anything to the serial, it will go to BOTH the computer and the controller. This should
+  // be fine, because the controller doesn't currently parse any input over BLE.
+
   // Motor setup
   pinMode(kRightWheel_Backwards, OUTPUT);
   pinMode(kRightWheel_Forwards, OUTPUT);
@@ -29,4 +41,10 @@ void setup() {
   pinMode(kLeftWheel_Backwards, OUTPUT);
 }
 
-void loop() {}
+void loop() {
+  if (Serial.available() > 0) {
+    processByteFromBle(Serial.read());
+  }
+
+  delay(kLoopDelay_ms);
+}
