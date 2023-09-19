1163e158bece2907f8fbddf4992fab45359f91d6
Matthew Allwright <matt.a.4890@gmail.com>
2023-09-18 20:13:42 -0600

    Add base for example robot code

---
+++ robot/robot.ino
@@ -0,0 +1,20 @@
+/**
+ * File: robot.ino
+ * Author: Matthew Allwright, theBasicBot
+ * Copyright: 2023
+ *
+ * Description:
+ * This file serves as an example for robots controlled by theBasicBot's ESP32-based controller.
+ */
+
+/* Constants ------------------------------------------------------------------------------------ */
+
+/* Variables ------------------------------------------------------------------------------------ */
+
+/* Functions ------------------------------------------------------------------------------------ */
+
+/* Setup and Loop ------------------------------------------------------------------------------- */
+
+void setup() {}
+
+void loop() {}
