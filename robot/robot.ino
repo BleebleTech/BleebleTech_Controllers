c4c83e904b0af6ed4b7ab70c3ff60920db087b71
Matthew Allwright <matt.a.4890@gmail.com>
2023-09-18 21:11:44 -0600

    Add message parsing to robot

--- robot/robot.ino
+++ robot/robot.ino
@@ -15,14 +15,52 @@ static constexpr int kRightWheel_Forwards = 5;
 static constexpr int kLeftWheel_Forwards = 6;
 static constexpr int kLeftWheel_Backwards = 11;
 
-// Generic configuration
-static constexpr int kLoopDelay_ms = 16;
+// Message decoding configuration
+static constexpr int kMessageSize_B = 7;
 
 /* Variables ------------------------------------------------------------------------------------ */
 
-/* Functions ------------------------------------------------------------------------------------ */
+static uint8_t rxBuffer[kMessageSize_B] = {};
+static size_t numRxBytes = 0;
 
-static void processByteFromBle(const uint8_t aRxByte) {}
+/* Functions ------------------------------------------------------------------------------------ */
+static void parseBleMessage() {
+  if (numRxBytes != kMessageSize_B) {
+    // The buffer doesn't contain the right number of bytes! We can't parse the message, since we
+    // don't know what is in the buffer.
+  } else {
+    // LEAVE THIS COMMENTED OUT IF YOU'RE NOT USING IT!
+    // Print out the buffer for the user to see
+    /*
+    char s[48] = {0};
+    snprintf(s, 47, "RX: [0x%02x][0x%02x][0x%02x][0x%02x][0x%02x][0x%02x][0x%02x]\n", rxBuffer[0],
+             rxBuffer[1], rxBuffer[2], rxBuffer[3], rxBuffer[4], rxBuffer[5], rxBuffer[6]);
+    Serial.println(s);
+    */
+  }
+}
+
+static void processByteFromBle(const uint8_t aRxByte) {
+  if (numRxBytes == 0 && aRxByte != 0xFF) {
+    // Ignore
+  } else if (numRxBytes < kMessageSize_B) {
+    rxBuffer[numRxBytes] = aRxByte;
+    numRxBytes++;
+  } else {
+    // We've somehow received too many bytes between start bytes. Clear the buffer and try again.
+    memset(rxBuffer, 0, sizeof(rxBuffer));
+    numRxBytes = 0;
+  }
+
+  if (numRxBytes == kMessageSize_B) {
+    // We've received enough information to parse out a full message
+    parseBleMessage();
+
+    // Clear the buffer to make room for the next message
+    memset(rxBuffer, 0, sizeof(rxBuffer));
+    numRxBytes = 0;
+  }
+}
 
 /* Setup and Loop ------------------------------------------------------------------------------- */
 
@@ -45,6 +83,4 @@ void loop() {
   if (Serial.available() > 0) {
     processByteFromBle(Serial.read());
   }
-
-  delay(kLoopDelay_ms);
 }
